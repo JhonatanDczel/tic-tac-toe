b464030935f32585e92f1dd52895da3b76fc53cb
JhonatanDczel <jariasq@unsa.edu.pe>
2023-12-06 15:23:26 -0500

    Agrega el controlador de juego

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 // Aqui es donde se controlan los datos del juego, como el tablero, los rounds jugados y demas
-function game() {
+const game = (() => {
   const board = [];
 
   for (let i = 0; i < 3; i++) {
     board[i] = [];
     for (let j = 0; j < 3; j++) {
       board[i][j] = Cell();
-
+      board[i][j].id = i * 3 + j;
+      console.log(board[i][j].id);
     }
   }
   let playerActual;
-}
+  console.log(board);
+
+  return { board };
+})();
 
 //Factory objets para los jugadores
 function player(playerName, playerToken) {
@@ -22,6 +26,7 @@ function player(playerName, playerToken) {
 //Generador de objetos para las celdas
 function Cell() {
   let value = 0;
+  let id;
 
   const addToken = (p) => {
     value = p.token;
@@ -37,6 +42,6 @@ function Cell() {
 //Esto controla todos losaspectos dejugabilidad del juego
 function gameControler() {
   function putToken(p, c) {
-
+    game.board[][]
   }
 }
