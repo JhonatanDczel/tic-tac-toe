d735e128b933b1298cf10b44caa7365dfa64ce87
JhonatanDczel <jariasq@unsa.edu.pe>
2023-12-06 15:09:44 -0500

    Agrega comentarios para el yo del futuro que vuelva a este proyecto

--- js/script.js
+++ js/script.js
@@ -1,12 +1,42 @@
+// Aqui es donde se controlan los datos del juego, como el tablero, los rounds jugados y demas
 function game() {
   const board = [];
+
+  for (let i = 0; i < 3; i++) {
+    board[i] = [];
+    for (let j = 0; j < 3; j++) {
+      board[i][j] = Cell();
+
+    }
+  }
   let playerActual;
 }
 
-function player() {
+//Factory objets para los jugadores
+function player(playerName, playerToken) {
   let wins = 0;
+  let token = playerName;
+  let name = playerToken;
 }
 
-function gameControler() {
+//Generador de objetos para las celdas
+function Cell() {
+  let value = 0;
 
+  const addToken = (p) => {
+    value = p.token;
+  };
+
+  const getToken = () => {
+    return value;
+  };
+
+  return { addToken, getToken };
+}
+
+//Esto controla todos losaspectos dejugabilidad del juego
+function gameControler() {
+  function putToken(p, c) {
+
+  }
 }
